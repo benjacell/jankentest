937f737abb848e0e6476e52aa27b3c8ab0adba16
Benjamin <purcell.benjamin.97@gmail.com>
2024-01-15 12:15:56 -0500

    create rng for rock paper scissors

---
+++ game.js
@@ -0,0 +1,15 @@
+function getRandomInInclusive(min, max) {
+min = Math.ceil(min);
+max = Math.floor(max);
+return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function compMove() {
+    const ans = getRandomInInclusive(1, 3);
+    if (ans == 1) {
+    return "Rock"
+    }
+    else if (ans == 2) {
+        return "Paper"
+    }
+    else {return "Scissors"}
+}
